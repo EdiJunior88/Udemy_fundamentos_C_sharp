a1f01fc65ca14d5d01db3eb8a267bf0a1b544b55
edijunior88 <er.moura88@yahoo.com.br>
2020-10-15 13:45:12 -0300

    Adicionar arquivos de projeto.

---
+++ DeclararVariaveis(2)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclararVariaveis_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var cor_favorita = "vermelho";
+            var modeloDoProduto = 2323;
+            */
+
+            dynamic cor_favorita = "Vermelho";
+
+            Console.WriteLine(cor_favorita);
+
+            cor_favorita = 2323232332;
+
+            Console.WriteLine(cor_favorita);
+
+            cor_favorita = 3235232.568f;
+
+            Console.WriteLine(cor_favorita);
+
+            Console.ReadLine();
+        }
+    }
+}
