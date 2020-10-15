f98539c0b9ab06725b354a75c1ecb14fcd538ba0
edijunior88 <er.moura88@yahoo.com.br>
2020-10-15 10:52:23 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroCodigoCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroCodigoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Meu nom é Edi Junior");
+            Console.ReadLine();
+        }
+    }
+}
