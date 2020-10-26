f0cb05cdc0dd0b728f40aee5664231132deb8c8b
EdiJunior88 <er.moura88@yahoo.com.br>
2020-10-26 13:56:42 -0300

    Adicionar arquivos de projeto.

---
+++ Constantes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float PI = 3.145565656f; //const não são variáveis e não podem ser modificados
+
+            Console.ReadLine();
+        }
+    }
+}
