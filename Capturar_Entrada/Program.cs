c5dd228a623e8c718596cacf4dfb8d3a5c1ff1c8
EdiJunior88 <er.moura88@yahoo.com.br>
2020-10-27 09:53:07 -0300

    Adicionar arquivos de projeto.

---
+++ Capturar_Entrada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capturar_Entrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: ");
+            Console.WriteLine(nome);
+
+            Console.ReadLine();
+        }
+    }
+}
