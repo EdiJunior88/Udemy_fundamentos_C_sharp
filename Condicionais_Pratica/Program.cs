72ce6856c0da4a3a2e40e134cd13bf83897640a9
EdiJunior88 <er.moura88@yahoo.com.br>
2020-10-27 15:46:35 -0300

    Adicionar arquivos de projeto.

---
+++ Condicionais_Pratica/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais_Pratica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 2;
+
+            if(a < b) //SE
+            {
+                Console.WriteLine("É verdade!");
+            }
+            else if (a > c)
+            {
+                Console.WriteLine("Executou Elseif!");
+            }
+            else //SE NÃO
+            {
+                Console.WriteLine("É mentira!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
