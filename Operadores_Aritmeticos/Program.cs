f62df779dd531ddc9a5cb2fc894f66a374c98f48
EdiJunior88 <er.moura88@yahoo.com.br>
2020-10-27 10:29:15 -0300

    Adicionar arquivos de projeto.

---
+++ Operadores_Aritmeticos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_Aritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroQualquer = 20 + 20 - 80;
+            int numeroMulti = 10 * 90;
+            float numeroDiv = 5.0f / 2;
+
+            int teste = (2 + 2) * 10;
+
+            Console.WriteLine(numeroQualquer);
+            Console.WriteLine(numeroMulti);
+            Console.WriteLine(numeroDiv);
+            Console.WriteLine(teste);
+
+            Console.ReadLine();
+        }
+    }
+}
