366409dda54cbcbf638047017593ca2f4e075d8d
EdiJunior88 <er.moura88@yahoo.com.br>
2020-11-03 15:38:26 -0300

    Adicionar arquivos de projeto.

---
+++ Operadores_Logicos_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_Logicos_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 0 && idade <= 11)
+            {
+                Console.WriteLine("Você é uma criança!");
+            }
+            else if (idade >= 12 && idade <= 18)
+            {
+                Console.WriteLine("Você é um adolescente!");
+            }
+            else if (idade >= 19 && idade <= 60)
+            {
+                Console.WriteLine("Você é um adulto!");
+            }
+            else
+            {
+                Console.WriteLine("Você é um idoso!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
