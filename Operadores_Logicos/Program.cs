91a3a1f1918593d8197177faa085aba138987b70
EdiJunior88 <er.moura88@yahoo.com.br>
2020-11-03 14:14:33 -0300

    Adicionar arquivos de projeto.

---
+++ Operadores_Logicos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_Logicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 20;
+            int c = 200;
+
+            if (a < b || a > c) //SE
+            {
+                Console.WriteLine("É verdade!");
+            }
+            else if (a > c)
+            {
+                Console.WriteLine("Executou ElseIf!");
+            }
+            else //SE NÃO
+            {
+                Console.WriteLine("É mentira!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
