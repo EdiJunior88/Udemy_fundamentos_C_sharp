578d7da5d2881edefc9e6da848274b13e83b006f
EdiJunior88 <er.moura88@yahoo.com.br>
2020-12-03 13:53:08 -0300

    Adicionar arquivos de projeto.

---
+++ Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Sea of Thieves";
+            string produto2 = "FIFA";
+            string produto3 = "Minecraft";
+            string produto4 = "Half-Life";
+            string produto5 = "Portal";
+            string produto6 = "CS";
+
+            string[] produtos = new string[5] {
+                "Sea of Thieves", // índice 0
+                "FIFA", // índice 1
+                "Minecraft", // índice 2
+                "Half-Life", // índice 3
+                "Portal" // índice 4
+            };
+
+            int[] valores =
+            {
+                40,
+                50,
+                60,
+                70,
+                20
+            };
+
+            produtos[1] = "FIFA 2021";
+
+            Console.WriteLine(produtos[1]);
+
+            Console.ReadLine();
+        }
+    }
+}
