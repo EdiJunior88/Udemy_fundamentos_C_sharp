0b4ae149daa44840e8ab07607bfedb6ff593ba11
EdiJunior88 <er.moura88@yahoo.com.br>
2020-12-11 16:07:08 -0300

    Adicionar arquivos de projeto.

---
+++ Enum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        enum Cor {Azul, Verde, Amarelo, Vermelho}
+        static void Main(string[] args)
+        {
+            Cor corFavorita = Cor.Vermelho;
+            Cor corFavoritaDaCarla = Cor.Azul;
+
+            Console.WriteLine((Cor)2);
+            Console.WriteLine(corFavoritaDaCarla);
+
+            Console.ReadLine();
+        }
+    }
+}
