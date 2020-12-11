4340f1d2374f5c5f6e2d55f347f4083a3ba8e201
EdiJunior88 <er.moura88@yahoo.com.br>
2020-12-11 16:52:30 -0300

    Adicionar arquivos de projeto.

---
+++ Enum_Switch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Switch
+{
+    enum Opcao { Criar = 1, Deletar, Editar, Listar, Atualizar}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecione uma das opções abaixo: ");
+            Console.WriteLine("1-Criar\n2-Detelar\n3-Editar\n4-Listar\n5-Atualizar");
+            int index = int.Parse(Console.ReadLine());
+
+            Opcao opcaoSelecionada = (Opcao)index;
+
+            switch (opcaoSelecionada)
+            {
+                case Opcao.Criar:
+                    Console.WriteLine("Você quer criar algo!");
+                    break;
+                case Opcao.Deletar:
+                    Console.WriteLine("DELETE! DELE IMEDIATAMENTE!");
+                    break;
+                case Opcao.Editar:
+                    Console.WriteLine("Editar é muito bom!");
+                    break;
+                default:
+                    Console.WriteLine("Opção não encontrada!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
