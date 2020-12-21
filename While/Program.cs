5faadd5868543b2469fd24719211229d78bd115f
EdiJunior88 <er.moura88@yahoo.com.br>
2020-12-21 14:01:08 -0300

    Adicionar arquivos de projeto.

---
+++ While/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            while (contador < 20) //Enquanto for verdade faça
+            {
+                Console.WriteLine(contador + 1);
+                Console.WriteLine("Rodando o while!");
+                //contador = contador + 1;
+                //contador += 1;
+                contador++;
+            }
+
+            Console.WriteLine("FIM DA LINHA!");
+            Console.ReadLine();
+        }
+    }
+}
