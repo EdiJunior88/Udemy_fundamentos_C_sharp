8e30b94ca6dc698e7113204475ac2d3b00628f78
EdiJunior88 <er.moura88@yahoo.com.br>
2020-12-21 16:29:09 -0300

    Adicionar arquivos de projeto.

---
+++ Foreach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = { "Victor", "Lima", "Guia do prog", "Formação", "Node", "Curso de C#", "Udemy!" };
+
+            foreach(string palavra in palavras) //Para cada (foreach) palavra no array palavras, repita o bloco de código
+            {
+                Console.WriteLine(palavra);
+            }
+
+            Console.WriteLine("FIM DA LINHA!");
+            Console.ReadLine();
+        }
+    }
+}
