a2314b077d208f621e5a4ae22908867c39244b4e
EdiJunior88 <er.moura88@yahoo.com.br>
2020-12-21 15:23:26 -0300

    Adicionar arquivos de projeto.

---
+++ Do_While/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador2 = 0;
+
+            do
+            {
+                Console.WriteLine("do while!");
+                contador2++;
+            }
+            while (contador2 < 20);
+
+            Console.WriteLine("FIM DA LINHA!");
+            Console.ReadLine();
+        }
+    }
+}
